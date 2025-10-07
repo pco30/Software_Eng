ffb8124ae1c7d077966796a730ba46b166b87040
Jeff Rose <jeffrose@bu.edu>
2025-10-07 14:31:57 -0400

    Initial commit for Visual Studio WinChangeMonitor project files

---
+++ WinChangeMonitorForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinChangeMonitor
+{
+    partial class WinChangeMonitorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbWhatToTrack = new System.Windows.Forms.GroupBox();
+            this.tcWhatToTrack = new System.Windows.Forms.TabControl();
+            this.tpFolders = new System.Windows.Forms.TabPage();
+            this.dgvFoldersToTrack = new System.Windows.Forms.DataGridView();
+            this.tbcolFolder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbcolIncludeSubdirs = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tpRegistry = new System.Windows.Forms.TabPage();
+            this.bPreInstall = new System.Windows.Forms.Button();
+            this.bwPreInstall = new System.ComponentModel.BackgroundWorker();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.bPostInstall = new System.Windows.Forms.Button();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.gbWhatToTrack.SuspendLayout();
+            this.tcWhatToTrack.SuspendLayout();
+            this.tpFolders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoldersToTrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbWhatToTrack
+            // 
+            this.gbWhatToTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbWhatToTrack.Controls.Add(this.tcWhatToTrack);
+            this.gbWhatToTrack.Location = new System.Drawing.Point(12, 12);
+            this.gbWhatToTrack.Name = "gbWhatToTrack";
+            this.gbWhatToTrack.Size = new System.Drawing.Size(776, 188);
+            this.gbWhatToTrack.TabIndex = 0;
+            this.gbWhatToTrack.TabStop = false;
+            this.gbWhatToTrack.Text = "What To Track";
+            // 
+            // tcWhatToTrack
+            // 
+            this.tcWhatToTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tcWhatToTrack.Controls.Add(this.tpFolders);
+            this.tcWhatToTrack.Controls.Add(this.tpRegistry);
+            this.tcWhatToTrack.Location = new System.Drawing.Point(6, 19);
+            this.tcWhatToTrack.Name = "tcWhatToTrack";
+            this.tcWhatToTrack.SelectedIndex = 0;
+            this.tcWhatToTrack.Size = new System.Drawing.Size(764, 163);
+            this.tcWhatToTrack.TabIndex = 0;
+            // 
+            // tpFolders
+            // 
+            this.tpFolders.Controls.Add(this.dgvFoldersToTrack);
+            this.tpFolders.Location = new System.Drawing.Point(4, 22);
+            this.tpFolders.Name = "tpFolders";
+            this.tpFolders.Padding = new System.Windows.Forms.Padding(3);
+            this.tpFolders.Size = new System.Drawing.Size(756, 137);
+            this.tpFolders.TabIndex = 0;
+            this.tpFolders.Text = "Folders";
+            this.tpFolders.UseVisualStyleBackColor = true;
+            // 
+            // dgvFoldersToTrack
+            // 
+            this.dgvFoldersToTrack.AllowUserToAddRows = false;
+            this.dgvFoldersToTrack.AllowUserToDeleteRows = false;
+            this.dgvFoldersToTrack.AllowUserToResizeColumns = false;
+            this.dgvFoldersToTrack.AllowUserToResizeRows = false;
+            this.dgvFoldersToTrack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvFoldersToTrack.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFoldersToTrack.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFoldersToTrack.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tbcolFolder,
+            this.cbcolIncludeSubdirs});
+            this.dgvFoldersToTrack.Location = new System.Drawing.Point(6, 6);
+            this.dgvFoldersToTrack.Name = "dgvFoldersToTrack";
+            this.dgvFoldersToTrack.RowHeadersVisible = false;
+            this.dgvFoldersToTrack.Size = new System.Drawing.Size(744, 125);
+            this.dgvFoldersToTrack.TabIndex = 0;
+            // 
+            // tbcolFolder
+            // 
+            this.tbcolFolder.DataPropertyName = "Folder";
+            this.tbcolFolder.HeaderText = "Folder";
+            this.tbcolFolder.Name = "tbcolFolder";
+            this.tbcolFolder.ReadOnly = true;
+            // 
+            // cbcolIncludeSubdirs
+            // 
+            this.cbcolIncludeSubdirs.DataPropertyName = "IncludeSubDirectories";
+            this.cbcolIncludeSubdirs.HeaderText = "Include Sub-Directories?";
+            this.cbcolIncludeSubdirs.Name = "cbcolIncludeSubdirs";
+            // 
+            // tpRegistry
+            // 
+            this.tpRegistry.BackColor = System.Drawing.Color.Red;
+            this.tpRegistry.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tpRegistry.Location = new System.Drawing.Point(4, 22);
+            this.tpRegistry.Name = "tpRegistry";
+            this.tpRegistry.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRegistry.Size = new System.Drawing.Size(756, 137);
+            this.tpRegistry.TabIndex = 1;
+            this.tpRegistry.Text = "Registry";
+            // 
+            // bPreInstall
+            // 
+            this.bPreInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bPreInstall.Location = new System.Drawing.Point(12, 206);
+            this.bPreInstall.Name = "bPreInstall";
+            this.bPreInstall.Size = new System.Drawing.Size(102, 23);
+            this.bPreInstall.TabIndex = 1;
+            this.bPreInstall.Text = "Perform Pre-Install";
+            this.bPreInstall.UseVisualStyleBackColor = true;
+            this.bPreInstall.Click += new System.EventHandler(this.bPreInstall_Click);
+            // 
+            // bwPreInstall
+            // 
+            this.bwPreInstall.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwPreInstall_DoWork);
+            this.bwPreInstall.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwPreInstall_RunWorkerCompleted);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutput.Location = new System.Drawing.Point(12, 235);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOutput.Size = new System.Drawing.Size(776, 203);
+            this.tbOutput.TabIndex = 2;
+            // 
+            // bPostInstall
+            // 
+            this.bPostInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bPostInstall.Enabled = false;
+            this.bPostInstall.Location = new System.Drawing.Point(120, 206);
+            this.bPostInstall.Name = "bPostInstall";
+            this.bPostInstall.Size = new System.Drawing.Size(107, 23);
+            this.bPostInstall.TabIndex = 3;
+            this.bPostInstall.Text = "Perform Post-Install";
+            this.bPostInstall.UseVisualStyleBackColor = true;
+            this.bPostInstall.Click += new System.EventHandler(this.bPostInstall_Click);
+            // 
+            // lStatus
+            // 
+            this.lStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lStatus.Location = new System.Drawing.Point(233, 211);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(555, 13);
+            this.lStatus.TabIndex = 0;
+            // 
+            // WinChangeMonitorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.bPostInstall);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.bPreInstall);
+            this.Controls.Add(this.gbWhatToTrack);
+            this.Name = "WinChangeMonitorForm";
+            this.ShowIcon = false;
+            this.Text = "WinChangeMonitor - BU MET CS 673/473 Group #3 - Fall 2025";
+            this.Load += new System.EventHandler(this.WinChangeMonitorForm_Load);
+            this.gbWhatToTrack.ResumeLayout(false);
+            this.tcWhatToTrack.ResumeLayout(false);
+            this.tpFolders.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFoldersToTrack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbWhatToTrack;
+        private System.Windows.Forms.TabControl tcWhatToTrack;
+        private System.Windows.Forms.TabPage tpFolders;
+        private System.Windows.Forms.DataGridView dgvFoldersToTrack;
+        private System.Windows.Forms.Button bPreInstall;
+        private System.ComponentModel.BackgroundWorker bwPreInstall;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tbcolFolder;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn cbcolIncludeSubdirs;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button bPostInstall;
+        private System.Windows.Forms.TabPage tpRegistry;
+        private System.Windows.Forms.Label lStatus;
+    }
+}
+
---
+++ WinChangeMonitorForm.cs
@@ -0,0 +1,314 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinChangeMonitor
+{
+    public partial class WinChangeMonitorForm : Form
+    {
+        private DataTable dtFoldersToTrack = new DataTable();
+        private DataTable dtRegistryKeysToTrack = new DataTable();
+        // List<String>.Contains(String) is O(n)
+        // SortedSet<String>.Contains(String) is O(log n)
+        // Dictionary<String, ...>.ContainsKey(String) approaches O(1)
+        private SortedDictionary<String, Boolean> folderContentsPreInstall = new SortedDictionary<String, Boolean>(); // key is full path to file/folder, value is whether this is a folder or file (true if folder, false if file)
+        private SortedDictionary<String, String> registryContentsPreInstall = new SortedDictionary<String, String>(); // TODO - enhance this so values are (Type [Key|ValueDataType], String)
+        private DateTime? preInstallStarted = null;
+        private DateTime? preInstallFinished = null;
+        private SortedDictionary<String, Boolean> folderContentsAdded = new SortedDictionary<String, Boolean>();
+        private SortedDictionary<String, Boolean> folderContentsModified = new SortedDictionary<String, Boolean>();
+
+        private void HandleException(Exception ex)
+        {
+            MessageBox.Show($"{ex.Message}{Environment.NewLine}{ex.StackTrace}");
+        }
+
+        public WinChangeMonitorForm()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void WinChangeMonitorForm_Load(Object sender, EventArgs e)
+        {
+            try
+            {
+                this.dtFoldersToTrack.Columns.Add("Folder");
+                this.dtFoldersToTrack.Columns.Add("IncludeSubDirectories");
+
+                this.dtFoldersToTrack.Rows.Add(@"C:\", false);
+
+                this.dgvFoldersToTrack.DataSource = this.dtFoldersToTrack;
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void bPreInstall_Click(Object sender, EventArgs e)
+        {
+            try
+            {
+                this.tbOutput.Clear();
+                this.preInstallStarted = DateTime.Now;
+                this.tbOutput.AppendText($"Pre-Install File/Folder Inventory Started @ {this.preInstallStarted}{Environment.NewLine}");
+
+                this.tcWhatToTrack.Enabled = false;
+                this.bPreInstall.Enabled = false;
+
+                this.bwPreInstall.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void InventoryDirectory(SortedDictionary<String, Boolean> inventory, String directory, Boolean recursive, Boolean isPreinstall = true)
+        {
+            try
+            {
+                ControlSetText(this.lStatus, directory);
+
+                try
+                {
+                    String[] files = Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly);
+
+                    foreach (String file in files)
+                    {
+                        if (isPreinstall)
+                        {
+                            inventory[file] = false;
+                        }
+                        else // !isPreinstall (this is a Post-Install Inventory/Diff)
+                        {
+                            if (!inventory.ContainsKey(file))
+                            {
+                                this.folderContentsAdded[file] = false;
+                            }
+                            else // both preinstall and postinstall inventories contain the file, check to see if it was modified
+                            {
+                                if (File.GetLastWriteTime(file) > this.preInstallFinished)
+                                {
+                                    this.folderContentsModified[file] = false;
+                                }
+
+                                inventory.Remove(file); // remove file from preinstall inventory so that all keys contained in inventory at the end were those removed by the tracked executable/script
+                            }
+                        }
+                    }
+
+                    String[] subDirectories = Directory.GetDirectories(directory, "*", SearchOption.TopDirectoryOnly);
+
+                    foreach (String subDirectory in subDirectories)
+                    {
+                        if (isPreinstall)
+                        {
+                            inventory[subDirectory] = true;
+                        }
+                        else // !isPreinstall (this is a Post-Install Inventory/Diff)
+                        {
+                            if (!inventory.ContainsKey(subDirectory))
+                            {
+                                this.folderContentsAdded[subDirectory] = true;
+                            }
+                            else // both preinstall and postinstall inventories contain the folder, so it's neither new nor deleted
+                            {
+                                if (Directory.GetLastWriteTime(subDirectory) > this.preInstallFinished)
+                                {
+                                    this.folderContentsModified[subDirectory] = true;
+                                }
+
+                                inventory.Remove(subDirectory); // remove subDirectory from preinstall inventory so that all keys contained in inventory at the end were those removed by the tracked executable/script
+                            }
+                        }
+
+                        if (recursive)
+                        {
+                            InventoryDirectory(inventory, subDirectory, recursive: true, isPreinstall: isPreinstall);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException) { }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        
+
+        private void InventoryRegistry(SortedDictionary<String, String> inventory, RegistryKey key, Boolean recursive)
+        {
+            try
+            {
+                String[] valueNames = key.GetValueNames();
+
+                foreach (String valueName in valueNames)
+                {
+                    if (!inventory.ContainsKey(valueName))
+                    {
+                        // TODO - inventory[valueName] = (key.GetValueKind(valueName), key.GetValue(valueName).ToString())
+                        inventory[valueName] = key.GetValue(valueName).ToString();
+                    }
+                }
+
+                String[] subkeyNames = key.GetSubKeyNames();
+
+                foreach (String subkeyName in subkeyNames)
+                {
+                    if (!inventory.ContainsKey(subkeyName))
+                    {
+                        inventory[subkeyName] = "Key";
+                    }
+
+                    if (recursive)
+                    {
+                        InventoryRegistry(inventory, key.OpenSubKey(subkeyName), true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        delegate void TextBoxClearDelegate(TextBox textBox);
+        private void TextBoxClear(TextBox textBox)
+        {
+            try
+            {
+                if (textBox.InvokeRequired)
+                {
+                    textBox.Invoke(new TextBoxClearDelegate(TextBoxClear), new Object[] { textBox });
+                }
+                else
+                {
+                    textBox.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        delegate void ControlSetTextDelegate(Control control, String text);
+        private void ControlSetText(Control control, String text)
+        {
+            try
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new ControlSetTextDelegate(ControlSetText), new Object[] { control, text });
+                }
+                else
+                {
+                    control.Text = text;
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        delegate void TextBoxAppendTextDelegate(TextBox textBox, String text);
+        private void TextBoxAppendText(TextBox textBox, String text)
+        {
+            try
+            {
+                if (textBox.InvokeRequired)
+                {
+                    textBox.Invoke(new TextBoxAppendTextDelegate(TextBoxAppendText), new Object[] { textBox, text });
+                }
+                else
+                {
+                    textBox.AppendText(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void bwPreInstall_DoWork(Object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+                foreach (DataRow row in this.dtFoldersToTrack.Rows)
+                {
+                    InventoryDirectory(this.folderContentsPreInstall, row["Folder"].ToString(), Boolean.Parse(row["IncludeSubDirectories"].ToString()));
+                }
+
+                ControlSetText(this.lStatus, "");;
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void bwPreInstall_RunWorkerCompleted(Object sender, RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                this.preInstallFinished = DateTime.Now;
+
+                TextBoxAppendText(this.tbOutput, $"Pre-Install File/Folder Inventory Finished @ {this.preInstallFinished}{Environment.NewLine}");
+
+                this.tbOutput.AppendText($"Time Elapsed: {this.preInstallFinished - this.preInstallStarted}{Environment.NewLine}");
+
+                TextBoxAppendText(this.tbOutput, $"Inventoried {this.folderContentsPreInstall.Keys.Count.ToString("N0")} items");
+
+
+
+                this.bPreInstall.Enabled = true;
+                this.bPostInstall.Enabled = true;
+                this.tcWhatToTrack.Enabled = true;
+
+                //foreach (String entry in this.folderContentsPreInstall.Keys)
+                //{
+                //    TextBoxAppendText(this.tbOutput, $"{Environment.NewLine}{entry}");
+                //}
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private void bPostInstall_Click(Object sender, EventArgs e)
+        {
+            try
+            {
+                //RegistryKey baseKey;
+                //foreach (DataRow row in this.dtRegistryKeysToTrack.Rows)
+                //{
+                //    if (row)
+                //}
+                //    = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                //InventoryRegistry(this.registryContentsPreInstall, hklm);
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+    }
+}
