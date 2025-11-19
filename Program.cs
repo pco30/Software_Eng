c495c43e63eeb06cea5023aa52b82fdb3b2f61d7
Jeff Rose <jeffrose@bu.edu>
2025-11-19 01:03:52 -0500

    prevent multiple program instances from running simultaneously

--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WinChangeMonitor
 {
     internal static class Program
     {
+        private static Mutex mutex = new Mutex(true, "WinChangeMonitor");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WinChangeMonitorForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new WinChangeMonitorForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("Another instance of WinChangeMonitor is already running.", "Multiple Instances Detected", MessageBoxButtons.OK);
+                Application.Exit();
+            }
         }
     }
 }
